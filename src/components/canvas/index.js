19d69e0f07a710dc025d3076262aa86d1b836928
MichMatrix <michel.a.s401@gmail.com>
2023-11-20 00:33:24 -0300

    feature: add 3d planet model and canvas folder for 3d components

---
+++ src/components/canvas/index.js
@@ -0,0 +1,7 @@
+import EarthCanvas from "./Earth";
+import BallCanvas from "./Ball";
+import ComputersCanvas from "./Computers";
+import StarsCanvas from "./Stars";
+import GraphCanvas from "./Graph";
+
+export { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas, GraphCanvas };
