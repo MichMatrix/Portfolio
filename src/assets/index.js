a1442508c917f436ec541bc306a3331c76a194bb
MichMatrix <michel.a.s401@gmail.com>
2023-11-20 00:38:20 -0300

    feature: add assets

---
+++ src/assets/index.js
@@ -0,0 +1,68 @@
+import logo from "./logo.svg";
+
+import creator from "./creator.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import data_analysts from "./data_analysts.png";
+import cloudIcon from "./cloudIcon.png"
+
+import css from "./tech/css.png";
+
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+import googleCloud from "./tech/googleCloud.svg";
+
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+import tnslatam from "./company/logoTNS.png";
+import chingu from "./company/logoChingu.png";
+
+import carrent from "./carrent.png";
+import flexTurism from "./flexTurism.png";
+import chuckster from "./chuckster.png";
+import portfolio from "./portfolio.png";
+
+export {
+  logo,
+  creator,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  shopify,
+  starbucks,
+  tesla,
+  carrent,
+  googleCloud,
+  data_analysts,
+  cloudIcon,
+  tnslatam,
+  chingu,
+  flexTurism,
+  chuckster,
+  portfolio,
+};
