c4cd15e346e7af17182c1e559c5df00fc023a94c
MichMatrix <michel.a.s401@gmail.com>
2023-11-20 02:17:09 -0300

    fix: update index import

--- src/components/canvas/index.js
+++ src/components/canvas/index.js
@@ -1,7 +1,6 @@
 import EarthCanvas from "./Earth";
 import BallCanvas from "./Ball";
-import ComputersCanvas from "./Computers";
 import StarsCanvas from "./Stars";
 import GraphCanvas from "./Graph";
 
-export { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas, GraphCanvas };
+export { EarthCanvas, BallCanvas, StarsCanvas, GraphCanvas };
